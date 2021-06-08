28420f2736bc3bcc8c1a80273ad878bc14274062
Ivan Kuzmenko <kuzmenko.ivan2002@yandex.com>
2021-06-09 01:12:28 +0300

    More HamVM & automatic FGD

--- code/entities/BaseAgent.cs
+++ code/entities/BaseAgent.cs
@@ -6,10 +6,34 @@ namespace CBTIO
 	public partial class BaseAgent : ModelEntity
 	{
 		[Net, Local]
-		protected byte[] Memory { get; set; } = new byte[256];
+		protected byte[] AddressSpace { get; set; } = new byte[0xffff];
+
+		protected uint MemorySize = 0x1000; // 0 <= addr < MemorySize
+
+		[Net, Local]
+		protected int[] Registers { get; set; } = new int[10];
+
+		protected readonly uint[] InterruptTable = new uint[8]
+			{
+				0xFF10,
+				0xFF12,
+				0xFF14,
+				0xFF16,
+				0xFF18,
+				0xFF1A,
+				0xFF1C,
+				0xFF1E
+			};
+
 
 		[Net, Local]
 		protected uint PC { get; set; } = 0;
+
+		[Net, Local]
+		protected uint SP { get; set; } = 0xFE00;
+
+		[Net, Local]
+		protected uint StackSize { get; set; } = 0xff;
 
 		[Net, Local]
 		protected bool ExternalTickSource { get; set; } = false;
@@ -32,24 +56,24 @@ namespace CBTIO
 
 		public virtual byte Peek(uint address)
 		{
-			if (address < Memory.Length)
-				return Memory[address];
-			throw new ArgumentOutOfRangeException($"Address out of bounds: expected 0 <= address < {Memory.Length}, got {address}");
+			if (address < AddressSpace.Length)
+				return AddressSpace[address];
+			throw new ArgumentOutOfRangeException($"Address out of bounds: expected 0 <= address < {AddressSpace.Length}, got {address}");
 		}
 
 		public virtual void Poke(uint address, byte value)
 		{
 
-			if ( address < Memory.Length )
-				Memory[address] = value;
-			throw new ArgumentOutOfRangeException( $"Address out of bounds: expected 0 <= address < {Memory.Length}, got {address}" );
+			if ( address < AddressSpace.Length )
+				AddressSpace[address] = value;
+			throw new ArgumentOutOfRangeException( $"Address out of bounds: expected 0 <= address < {AddressSpace.Length}, got {address}" );
 		}
 
 		public virtual void BCTick()
 		{
-			if ( PC >= Memory.Length )
+			if ( PC >= AddressSpace.Length )
 				PC = 0;
-			TryDecode( Memory[PC] );
+			TryDecode( AddressSpace[PC] );
 		}
 
 		/// <summary>
@@ -59,15 +83,38 @@ namespace CBTIO
 		// TODO: implement full Hamster architecture https://github.com/rndtrash/hamster
 		public virtual void TryDecode(byte opcode)
 		{
-			switch (opcode & 0xf0 >> 4)
+			byte category = (byte)(opcode & 0xf0 >> 4);
+			byte instruction = (byte)(opcode & 0x0f);
+
+			switch (category)
+			{
+				case 0:
+					HamVM_General(instruction);
+					return;
+				//
+				default:
+					throw new ArgumentException( $"Invalid opcode: {opcode}" );
+			}
+		}
+
+		protected void HamVM_General(byte instruction)
+		{
+			switch (instruction)
 			{
 				case 0:
 					Log.Info( "Hamster: noop" );
 					PC++;
 					return;
-				//
+				case 1:
+					Log.Error( "TODO: halt and catch fire" );
+					PC++;
+					return;
+				case 2:
+					Log.Error( "TODO: jmp to interrupt from it and save prev pos" );
+					PC++;
+					return;
 				default:
-					throw new ArgumentException( $"Invalid opcode: {opcode}" );
+					throw new ArgumentException( $"Invalid general instruction: {instruction}" );
 			}
 		}
 	}
